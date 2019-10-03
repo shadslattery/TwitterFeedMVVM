ab649a129db2e7caf7076c08cef381e2aa377b5a
shadslattery <shadslattery@gmail.com>
2019-10-02 23:15:14 -0400

    Initial commit

---
+++ app/src/main/java/com/example/twitterfeedmvvm/view/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.twitterfeedmvvm.view;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+/*
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+*/
+
+import com.example.twitterfeedmvvm.R;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    }
+
